43c28f38580cf02e4d60f52412b858a7e4dd1313
Jacob Magnuson <jacob@cryptoctf.com>
2016-05-16 08:05:31 +0000

    removed threefold

--- problems/threefold/gen.py
+++
@@ -1,4 +0,0 @@
-flag = 'flag{three_is_a_magic_numb3r_3333333}'
-while len(flag) < 5 * 10**7:
-    flag = flag.encode('hex')
-open('several_threes.txt', 'w').write(flag)
--- problems/threefold/sol.py
+++
@@ -1,4 +0,0 @@
-problem = open('several_threes.txt', 'r').readline()
-while '{' not in problem:
-    problem = problem.decode('hex')
-print problem
