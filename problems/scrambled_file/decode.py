e1c14daabc8a7f62bd0c086ab581547a6b9a8fa3
MegaAbsol <kevinshen100@gmail.com>
2016-05-22 18:15:29 -0400

    fixed scrambled file
    fixed scrambled file

--- problems/scrambled_file/decode.py
+++ problems/scrambled_file/decode.py
@@ -1,9 +1,9 @@
-a = open('synt.7z','rb')
+a = open('wut','rb')
 data = a.read()
 a.close()
 #out = open('out','wb')
 for byte in data:
     #print(hex(byte))
     #print(hex((byte+13)%0xff))
-    print(hex((byte+13)%256).lstrip('0x').zfill(2),end='')
+    print(hex((byte-13)%255).lstrip('0x').zfill(2),end='')
 #out.close()
