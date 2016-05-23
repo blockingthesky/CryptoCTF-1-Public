12c4b7db32667b93852a0c17f3516aab9f22fcff
blockingthesky <Jmagnuson@live.com>
2016-05-22 21:43:03 -0400

    Added Psychic 2

--- problems/psychic_2/gen.py
+++ problems/psychic_2/gen.py
@@ -1,5 +1,22 @@
 import random
 
-def gen_reducable_string(final):
-    root = random.randint(0, 255)
-    nums = [root] + 
+def xor_pair(num):
+    k = random.randint(0, 255)
+    return k, num ^ k
+
+def gen_reduceable_string(final):
+    ret = [final]
+    size = random.randint(10000, 20000)
+    for i in xrange(size):
+        a, b = xor_pair(ret[-1])
+        ret = ret[:-1] + [a, b]
+    return ''.join(hex(u)[2:].zfill(2) for u in ret)
+
+flag = "in the spirit of psychic problems, here's a Caesared flag: pvkq{e_bokn_wi_wsxn_agbslsfigso}"
+for part in range(len(flag)):
+    print part
+    out = open('psychic_2_files/psychic2_' + str(part) + '.lol', 'w')
+    out.write(gen_reduceable_string(ord(flag[part])))
+    out.close()
+print len(flag)
+    
--- problems/psychic_2/sol.py
+++ problems/psychic_2/sol.py
@@ -1,17 +1,9 @@
-import string, math
+import operator
 
-primes = filter(lambda x: not any(x % i == 0 for i in range(2,int(math.sqrt(x) + 1))), range(2,10000))
-
-def rot(let, amt):
-    if let not in string.ascii_letters:
-        return let
-    k = ord(let)
-    if k > 96:
-        k -= 97
-        return chr((k - amt) % 26 + 97)
-    k -= 65
-    return chr((k - amt) % 26 + 65)
-
-ciphertext = open('decryptme', 'r').readline()
-
-print ''.join(rot(ciphertext[i], primes[i]) for i in range(len(ciphertext)))
+flag = ''
+for i in range(91):
+    inp = open('psychic_2_files/psychic2_' + str(i) + '.lol', 'r').readline().decode('hex')
+    inp = [ord(u) for u in inp]
+    flag += chr(reduce(operator.xor, inp))
+print flag
+    
